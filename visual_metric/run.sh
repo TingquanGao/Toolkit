13f0e0b46c35eb7ab4209ae67f8742d4dc40daba
gaotingquan <gaotingquan@baidu.com>
2023-10-11 09:12:42 +0000

    add hgnetv2

--- visual_metric/run.sh
+++ visual_metric/run.sh
@@ -1,1 +1,1 @@
-python main.py --csv cnn_server_filterd.csv --graphs "Performance of the CNN server models:3,2" --specified "PP-HGNet_ssld:red,*" "PP-HGNet:red,o"
+python main.py --csv raw_data/cnn_server_filterd.csv --graphs "Performance of the CNN server models:3,2" --specified "PP-HGNetV2:red,*" "PP-HGNet:orange,o"
